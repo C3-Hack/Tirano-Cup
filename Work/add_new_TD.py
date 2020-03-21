9f3d90a6e192cf14826325738ff5ed3b101f263d
I-My-Prog <eitaroumo@gmail.com>
2020-03-21 19:48:13 +0900

    Change indicate rule of members

--- Work/add_new_TD.py
+++ Work/add_new_TD.py
@@ -2,20 +2,19 @@ import sqlite3
 #========================================
 #テストデータ
 projectname = "MTKirara"
-person = ['Akata','Naga','GAN']
+person = ['Akata','Naga','GAN','Hage','Gomi']
 todo = "make"
 count = 1
 #========================================
 
 con = sqlite3.connect('td.db')
 c = con.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS td_list(ProjectName,TODO_ID,member)''')
+c.execute('''CREATE TABLE IF NOT EXISTS td_list(ProjectName,TODO_ID,member1,member2,member3,member4,member5)''')
 
 #todo_ID = lambda c: chr(c+64)
 #todo_ID(count)
 todo_ID = "A"
-member = ','.join(person)    
 
-c.execute("INSERT INTO td_list VALUES(?,?,?)",[projectname,todo_ID,member])
+c.execute("INSERT INTO td_list VALUES(?,?,?,?,?,?,?)",[projectname,todo_ID,person[0],person[1],person[2],person[3],person[4]])
 con.commit()
 con.close()
