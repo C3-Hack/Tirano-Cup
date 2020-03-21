2aa02c39dec5ac5c2ad0f4694765e16b42e4800d
I-My-Prog <eitaroumo@gmail.com>
2020-03-21 19:54:31 +0900

    Use lambda formula

--- Work/add_new_TD.py
+++ Work/add_new_TD.py
@@ -4,17 +4,17 @@ import sqlite3
 projectname = "MTKirara"
 person = ['Akata','Naga','GAN','Hage','Gomi']
 todo = "make"
-count = 1
+count = 27
 #========================================
 
 con = sqlite3.connect('td.db')
 c = con.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS td_list(ProjectName,TODO_ID,member1,member2,member3,member4,member5)''')
 
-#todo_ID = lambda c: chr(c+64)
-#todo_ID(count)
-todo_ID = "A"
+todo_ID = lambda c: chr(c+64)
+id = todo_ID(count)
+#todo_ID = "A"
 
-c.execute("INSERT INTO td_list VALUES(?,?,?,?,?,?,?)",[projectname,todo_ID,person[0],person[1],person[2],person[3],person[4]])
+c.execute("INSERT INTO td_list VALUES(?,?,?,?,?,?,?)",[projectname,id,person[0],person[1],person[2],person[3],person[4]])
 con.commit()
 con.close()
