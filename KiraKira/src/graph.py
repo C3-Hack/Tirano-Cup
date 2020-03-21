7d8563b4ce527d2eb2608d135a9c58931ef90bef
naga-naga <z8pri2hr2ezjx0pith4e@docomo.ne.jp>
2020-03-21 21:47:52 +0900

    始点となる回数が2番目に多いノードにも色付けをする

--- KiraKira/src/graph.py
+++ KiraKira/src/graph.py
@@ -30,6 +30,8 @@ for row in c:
     # 始点の回数を+1
     counter[str(row[0])] += 1
 
+# counterのvaluesを集めて昇順に並び変え
+cnt_num = sorted(set(counter.values()))
 
 # select文をもう一回実行
 # 始点と終点を取得
@@ -38,8 +40,38 @@ c.execute("select start, end from nodes;")
 # select で取ってきた各行に対して実行
 for row in c:
     # 始点となる回数が最大のノードに色付け
-    if counter[row[0]] == max(counter.values()):
+    if counter[row[0]] == cnt_num[-1]:
         # 始点を強調する
+        graph.attr("node", color="red")
+        graph.node(str(row[0]), color="black", style="filled", fillcolor="red", fontcolor="black")
+        # 終点はそのまま
+        graph.attr("node", color="black")
+        graph.node(str(row[1]))
+    
+    # 始点となる回数が2番目に大きいノードに色付け
+    elif counter[row[0]] == cnt_num[-2]:
+        # 始点を強調する
+        graph.attr("node", color="darkorange")
+        graph.node(str(row[0]), color="black", style="filled", fillcolor="darkorange", fontcolor="black")
+        # 終点はそのまま
+        graph.attr("node", color="black")
+        graph.node(str(row[1]))
+    
+    # その他
+    else:
+        # そのままにする
+        graph.attr("node", color="black")
+
+    # 辺の作成
+    graph.edge(str(row[0]), str(row[1]))
+
+"""
+# 色づけしたいノード
+with_color = ["A", "B", "I", "O", "R", "S"]
+
+for row in c:
+    if str(row[0]) in with_color:
+    # 始点を強調する
         graph.attr("node", color="red")
         graph.node(str(row[0]), style="filled", fillcolor="red", fontcolor="black")
         # 終点はそのまま
@@ -48,9 +80,9 @@ for row in c:
     else:
         # そのままにする
         graph.attr("node", shape="circle", color="black")
-
-    # 辺の作成
+    
     graph.edge(str(row[0]), str(row[1]))
+"""
 
 # DB閉じる
 conn.commit()
